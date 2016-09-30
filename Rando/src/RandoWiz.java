dd49489397c3c69c4fe5379d3b8a6a307bcdb3bd
Teddy Todorov <forteddyt@gmail.com>
2016-09-30 16:41:39 -0400

    Practice problem added

--- Rando/src/RandoWiz.java
+++ Rando/src/RandoWiz.java
@@ -6,6 +6,24 @@ public class RandoWiz
 
     public static void main(String[] args)
     {
+        RandoWiz r = new RandoWiz();
+
+        r.unknown(4);
+
+        r.enumsInMaps();
+    }
+
+
+    void unknown(int n)
+    {
+        if (n > 0)
+            unknown(n - 1);
+        System.out.print("?");
+    }
+
+
+    public void enumsInMaps()
+    {
         Map<GenericEnum, String> map = new TreeMap<GenericEnum, String>();
         System.out.println(map.get(GenericEnum.type1));
         map.put(GenericEnum.type2, "Type 2");
