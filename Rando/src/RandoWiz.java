f3eeb5b7772736faa37066c103c5a55c91e22e7c
Teddy Todorov <forteddyt@gmail.com>
2016-12-10 22:16:09 -0500

    .classpath and RandoWiz.java changes

--- Rando/src/RandoWiz.java
+++ Rando/src/RandoWiz.java
@@ -1,29 +1,34 @@
 import java.util.Map;
 import java.util.TreeMap;
 
-public class RandoWiz
-{
+public class RandoWiz {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         RandoWiz r = new RandoWiz();
 
-        r.unknown(4);
+//        r.unknown(4);
 
-        r.enumsInMaps();
+//        r.enumsInMaps();
+        
+//        r.stringRecur("");
     }
 
 
-    void unknown(int n)
-    {
+    public void stringRecur(String s) {
+        if (s.length() < 15)
+            System.out.println(s);
+        stringRecur(s + "*");
+    }
+
+
+    void unknown(int n) {
         if (n > 0)
             unknown(n - 1);
         System.out.print("?");
     }
 
 
-    public void enumsInMaps()
-    {
+    public void enumsInMaps() {
         Map<GenericEnum, String> map = new TreeMap<GenericEnum, String>();
         System.out.println(map.get(GenericEnum.type1));
         map.put(GenericEnum.type2, "Type 2");
